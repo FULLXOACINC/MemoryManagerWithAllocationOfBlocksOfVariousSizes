d44de3139353cafb2e7befeff24a3759dca187b9
Alex <san91130324@gmail.com>
2017-09-04 00:14:26 +0300

    start lab

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "mmemory.h"
+
+int main() {
+    printf("Hello, World!\n");
+    printf("%s", isPowOfTwo(4) ? "true" : "false");
+    return 5;
+}
---
+++ mmemory.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "mmemory.h"
+#include <stdbool.h>
+
+
+#define SUCCESSFUL_CODE  0;
+#define INCORRECT_PARAMETERS_ERROR -1;
+#define NOT_ENOUGH_MEMORY_ERROR -2;
+#define OUT_OF_RANGE_ERROR -2;
+#define UNKNOWN_ERROR 1;
+
+int _malloc (VA* ptr, size_t szBlock){
+    return 0;
+}
+
+
+int __init (int n, int szPage){
+    printf("Hello, World!\n");
+
+    return n*szPage;
+}
+
+
